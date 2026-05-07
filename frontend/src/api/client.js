9dabdb56e50d9e8fda22ecebf0c13f8d6418b309
CManiMukesh <mani.vespucci@gmail.com>
2026-05-07 15:15:36 +0530

    a

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL + '/api',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -11,12 +11,15 @@ const api = axios.create({
 export const fetchPosts = async (before = null, limit = 10) => {
   const params = { limit };
   if (before) params.before = before;
+
   const response = await api.get('/posts', { params });
+
   return response.data;
 };
 
 export const createPost = async (content) => {
   const response = await api.post('/posts', { content });
+
   return response.data;
 };
 
